0e191a6a2808c943dae11f62137bffba631ef740
CHJ <09hyen28@naver.com>
2024-07-18 17:24:56 +0900

    feat: 음성찾기 컨트롤러 생성

--- frontend/src/components/RecordFile.vue
+++ frontend/src/components/RecordFile.vue
@@ -10,7 +10,7 @@ const props = defineProps({
     <div style="background-color: lightgray; border-radius: 5px; height: 80px; display: flex;">
         <div style="min-width: 95%; max-width: 95%; display: flex; flex-direction: row; align-items: center;">
             <div style="min-width: 40%; max-width: 40%;">
-                <img :src="props.item.imgUrl" style="border: 2px solid gray; padding: 2%;  min-width: 90%; max-width: 90%;  height:70px; border-radius: 5%;" />
+                <img :src="props.item.imageUrl" style="border: 2px solid gray; padding: 2%;  min-width: 90%; max-width: 90%;  height:70px; border-radius: 5%;" />
             </div>
             {{ item.title }}
         </div>
--- frontend/src/views/PlayListVue.vue
+++ frontend/src/views/PlayListVue.vue
@@ -14,8 +14,9 @@
 <script setup>
 import RecordFile from '@/components/RecordFile.vue';
 import { ref } from 'vue';
-import { reactive } from 'vue';
+import { reactive, onMounted } from 'vue';
 import { useRouter } from "vue-router";
+import axios from 'axios';
 
 const router = useRouter();
 
@@ -23,45 +24,19 @@ const title = ref('주워 듣기(실시간 알람함)');
 
 const state = reactive({
     title: '재생목록',
-    items: [
-        {
-            title: '부들부들 솜사탕 썰 푼다',
-            imgUrl: 'https://png.pngtree.com/thumb_back/fw800/background/20231008/pngtree-d-render-of-a-golden-tooth-exploring-dental-and-medical-concepts-image_13574408.png',
-            recordId: 123,
-        },
-        {
-            title: '부장님이랑 원펀치 뜬 썰 푼다',
-            imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXobLcA4-7fiJkSmqBaQnd2QL8gY8EFIx4pQ&s',
-            recordId: 123,
-        },
-        {
-            title: '갈매미가 내 돈 뺏어감',
-            imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgnrFfdTo9x-dksemfsNqIVUytfkP6mKDUiODsYF8Q&s',
-            recordId: 123,
-        },
-        {
-            title: '부들부들 솜사탕 썰 푼다',
-            imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE3Sghlf91X3SuEznW--hL06WkQRzOC3yBKg&s',
-            recordId: 123,
-        },
-        {
-            title: '부장님이랑 원펀치 뜬 썰 푼다',
-            imgUrl: 'https://png.pngtree.com/thumb_back/fw800/background/20231008/pngtree-d-render-of-a-golden-tooth-exploring-dental-and-medical-concepts-image_13574408.png',
-            recordId: 123,
-        },
-        {
-            title: '갈매미가 내 돈 뺏어감',
-            imgUrl: 'https://png.pngtree.com/thumb_back/fw800/background/20231008/pngtree-d-render-of-a-golden-tooth-exploring-dental-and-medical-concepts-image_13574408.png',
-            recordId: 123,
-        },
-        {
-            title: '갈매미가 내 돈 뺏어감',
-            imgUrl: 'https://png.pngtree.com/thumb_back/fw800/background/20231008/pngtree-d-render-of-a-golden-tooth-exploring-dental-and-medical-concepts-image_13574408.png',
-            recordId: 123,
-        },
-    ]
+    items: []
 
 });
+
+onMounted(async () =>{
+    try{
+        const response = await axios.get("http://localhost:8080/api-virus/virus")
+        state.items = response.data
+        console.log(state.items)
+    } catch(error){
+        console.error('Error fetching items', error)
+    }
+})
 
 function playRecord(id) {
   alert(`Image clicked: `+id);
