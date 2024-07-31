64127e4a90c2d8a6ece1d62ba426edb8dd8daf02
CHJ <09hyen28@naver.com>
2024-07-31 11:18:21 +0900

    Feat: navbar - 항목 클릭 당시 해당 페이지에 있는 경우 스크롤 최상단으로 이동

--- backend/src/main/java/com/ssafy/a505/controller/StorageController.java
+++ backend/src/main/java/com/ssafy/a505/controller/StorageController.java
@@ -21,26 +21,8 @@ public class StorageController {
     @GetMapping("/all")
     public ResponseEntity<?> findAll(){
         log.info("findAll");
-        Voice voice1 = new Voice();
-        voice1.setTitle("tst1");
-        Voice voice2 = new Voice();
-        voice2.setTitle("tst2");
-        Voice voice3 = new Voice();
-        voice3.setTitle("tst3");
-        Voice voice4 = new Voice();
-        voice4.setTitle("tst4");
-        Voice voice5 = new Voice();
-        voice5.setTitle("tst5");
-        Voice voice6 = new Voice();
-        voice6.setTitle("tst6");
 
-        List<Voice> result = new ArrayList<>();
-        result.add(voice1);
-        result.add(voice2);
-        result.add(voice3);
-        result.add(voice4);
-        result.add(voice5);
-        result.add(voice6);
+        List<Voice> result = makeDummy("spread");
 
         return new ResponseEntity<List<Voice>>(result, HttpStatus.OK);
     }
@@ -49,27 +31,19 @@ public class StorageController {
     public ResponseEntity<?> findLike(){
         log.info("findLike");
 
-        Voice voice1 = new Voice();
-        voice1.setTitle("like1");
-        Voice voice2 = new Voice();
-        voice2.setTitle("like2");
-        Voice voice3 = new Voice();
-        voice3.setTitle("like3");
-        Voice voice4 = new Voice();
-        voice4.setTitle("like4");
-        Voice voice5 = new Voice();
-        voice5.setTitle("like5");
-        Voice voice6 = new Voice();
-        voice6.setTitle("like6");
-
-        List<Voice> result = new ArrayList<>();
-        result.add(voice1);
-        result.add(voice2);
-        result.add(voice3);
-        result.add(voice4);
-        result.add(voice5);
-        result.add(voice6);
+        List<Voice> result = makeDummy("like");
 
         return new ResponseEntity<List<Voice>>(result, HttpStatus.OK);
     }
+
+    public List<Voice> makeDummy(String str){
+        List<Voice> result = new ArrayList<>();
+        for(int i = 1; i <= 20; i++){
+            Voice voice = new Voice();
+            voice.setTitle(str + i);
+            result.add(voice);
+        }
+        return result;
+    }
+
 }
