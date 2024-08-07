5e6b0cfa8c25dda8a29148a2ea70242fea19779c
whyWhale <soccer_by@naver.com>
2024-08-07 14:03:02 +0900

    Fix: 데이터 목록 가져오기 (인기음성, 좋아요음성, 알람음성)

--- backend/src/main/java/com/ssafy/a505/domain/repository/VoiceRepository.java
+++ backend/src/main/java/com/ssafy/a505/domain/repository/VoiceRepository.java
@@ -1,24 +1,23 @@
 package com.ssafy.a505.domain.repository;
 
-import com.ssafy.a505.domain.entity.Voice;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
+import com.ssafy.a505.domain.entity.Voice;
 
 public interface VoiceRepository extends JpaRepository<Voice, Long> {
     List<Voice> findByTitleContaining(String userNam, Pageable pageable);
 
     List<Voice> findAllByTitle(String title, Pageable pageable);
 
-    //    List<Voice> findAllByOrderByHeartCountDesc(Pageable pageable);
     @Query("SELECT v FROM Voice v WHERE v.dateTime >= :dateTime ORDER BY v.heartCount DESC")
-    List<Voice> findAllByOrderByHeartCountDesc(LocalDateTime dateTime, Pageable pageable);
+    List<Voice> findAllByOrderByHeartCountDesc(@Param("dateTime") LocalDateTime dateTime, Pageable pageable);
 
     @Query("SELECT v FROM Voice v WHERE v.title LIKE %:keyword%")
     Page<Voice> findVoicesWithKeyword(@Param("keyword") String keyword, Pageable pageable);
@@ -27,11 +26,9 @@ public interface VoiceRepository extends JpaRepository<Voice, Long> {
 
     List<Voice> findAllByMember_MemberId(Long memberId, Pageable pageable);
 
-    // 특정 유저의 좋아요 누른 음성 반환
     @Query("select v from Voice v join Heart h on v.voiceId = h.voice.voiceId where h.member.memberId = :memberId")
-    List<Voice> findByMemberWithHeart(Long memberId, Pageable pageable);
+    List<Voice> findByMemberWithHeart(@Param("memberId") Long memberId, Pageable pageable);
 
-    // 특정 유저가 주은 확산 음성 반환
     @Query("select v from Voice v join Spread s on v.voiceId = s.voice.voiceId where s.member.memberId = :memberId")
-    List<Voice> findByMemberWithSpread(Long memberId, Pageable pageable);
+    List<Voice> findByMemberWithSpread(@Param("memberId") Long memberId, Pageable pageable);
 }
